ea251ae5ac035eda94c1f7da1b2bd22bc7546cb2
jinDeHao <o.idhmaid@gmail.com>
2024-01-29 06:19:43 +0100

    doc

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Deletion-resilient hypermedia pagination """
         datakey = self.indexed_dataset()
         assert isinstance(index, int) and index < len(datakey)
         assert isinstance(page_size, int) and page_size > 0
