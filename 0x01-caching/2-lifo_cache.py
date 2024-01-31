40df25b0e703c27b3dce219669a2ce08f07ba89e
jinDeHao <o.idhmaid@gmail.com>
2024-02-01 00:22:38 +0100

    2 done

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python3
-from base.base_caching import BaseCaching
+"""Basic dictionary """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    inherits from BaseCaching
+    """
+    last_added = ""
+
+    def __init__(self):
+        """
+        initization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.last_added = key
+            return
+        keys = list(self.cache_data.keys())
+        if len(keys) >= BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(self.last_added))
+            del self.cache_data[self.last_added]
+        self.cache_data[key] = item
+        self.last_added = key
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
