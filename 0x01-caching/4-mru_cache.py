a0ada50f0b831835d2c0f6f9f37d82997aff17d9
jinDeHao <o.idhmaid@gmail.com>
2024-02-01 00:46:24 +0100

    4 done

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,2 +1,39 @@
 #!/usr/bin/env python3
-from base.base_caching import BaseCaching
+"""Basic dictionary """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        initization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            return
+        keys = list(self.cache_data.keys())
+        if len(keys) >= BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(keys[-1]))
+            del self.cache_data[keys[-1]]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        tmp = self.cache_data[key]
+        del self.cache_data[key]
+        self.cache_data[key] = tmp
+        return self.cache_data[key]
