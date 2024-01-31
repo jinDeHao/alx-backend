8a888f1cd0a44b45a04ca83530bcf8389d3c30fd
jinDeHao <o.idhmaid@gmail.com>
2024-01-31 23:17:29 +0100

    task 0 and others base class

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+from .base.base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BaseCache defines:
+      - inherts from BaseCaching
+    """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,2 @@
 #!/usr/bin/python3
+from .base.base_caching import BaseCaching
