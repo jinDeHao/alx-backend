7ed00eb28adf0e2e2d6defa35d2f25d4375afba3
jinDeHao <o.idhmaid@gmail.com>
2024-02-06 23:40:43 +0100

    all config

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -15,9 +15,12 @@ class Config:
     config for your Flask app
     """
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
+
 
 @app.route('/')
 def main():
