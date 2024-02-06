7a148b2b3160a61e82a4952c9884412c37f67420
jinDeHao <o.idhmaid@gmail.com>
2024-02-06 23:20:23 +0100

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+
+babel.default_locale = Config.LANGUAGES[0]
+babel.default_timezone = 'UTC'
+
+@app.route('/')
+def main():
+    """
+    simply outputs Welcome to Holberton
+    """
+    return render_template('1-index.html')
