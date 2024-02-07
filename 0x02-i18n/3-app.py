c09feed5cdc49cc86ed18088ee85fa4e390570e6
jinDeHao <o.idhmaid@gmail.com>
2024-02-07 01:19:08 +0100

    ayoha al babelyo la3in

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    config for your Flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Get locale from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def main():
+    """
+    simply outputs Welcome to Holberton
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
