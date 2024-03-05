69008e4f082e2c77d8ce7145726165e38fdb4373
jinDeHao <o.idhmaid@gmail.com>
2024-03-05 22:45:05 +0100

    done with 12 & 11

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -3,6 +3,13 @@ import kue from 'kue';
 import createPushNotificationsJobs from './8-job';
 
 describe('createPushNotificationsJobs', () => {
+  before(() => queue.testMode.enter());
+  afterEach(() => {
+    queue.testMode.clear();
+  });
+  after(() => {
+    queue.testMode.exit();
+  });
   it('display a error message if jobs is not an array', () => {
     const queue = kue.createQueue();
     kue.Job.rangeByState('inactive', 0, -1, 'asc', (err, jobs) => {
@@ -14,5 +21,35 @@ describe('createPushNotificationsJobs', () => {
       'Jobs is not an array'
     );
     queue.shutdown();
+  });
+  it('Testing the log', () => {
+    const consoleLog = sinon.spy(console, 'log');
+    const job = [
+      {
+        phoneNumber: '569865867',
+        message: 'The verification code is 9627',
+      },
+    ];
+    createPushNotificationsJobs(job, queue);
+
+    expect(consoleLog.args[0][0]).to.includes('Notification job created:');
+  });
+  it('Testing the type of jobs', () => {
+    const createSpy = sinon.spy(queue, 'create');
+    sinonList.push(createSpy);
+    const jobs = [
+      {
+        phoneNumber: '569865867',
+        message: 'The verification code is 9657',
+      },
+      {
+        phoneNumber: '569865867',
+        message: 'The verification code is 9627',
+      },
+    ];
+    createPushNotificationsJobs(jobs, queue);
+    jobs.forEach((job) => {
+      expect(createSpy.calledWith('push_notification_code_3', job)).to.be.true;
+    });
   });
 });
--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -1,1 +1,106 @@
-#!/usr/bin/node
+import express from 'express';
+import redis from 'redis';
+import { promisify } from 'util';
+
+const app = express();
+const client = redis.createClient();
+const getAsync = () => promisify(client.get).bind(client);
+const setAsync = () => promisify(client.set).bind(client);
+
+const listProducts = [
+  {
+    Id: 1,
+    name: 'Suitcase 250',
+    price: 50,
+    stock: 4,
+  },
+  {
+    Id: 2,
+    name: 'Suitcase 450',
+    price: 100,
+    stock: 10,
+  },
+  {
+    Id: 3,
+    name: 'Suitcase 650',
+    price: 350,
+    stock: 2,
+  },
+  {
+    Id: 4,
+    name: 'Suitcase 1050',
+    price: 550,
+    stock: 5,
+  },
+  {
+    Id: 5,
+    name: 'Suitcase 1950',
+    price: 550,
+    stock: 0,
+  },
+];
+
+function getItemById(id) {
+  const item = listProducts.filter((elem) => elem.Id === id);
+  return item;
+}
+
+async function reserveStockById(itemId, stock) {
+    const key = `item.${itemId}`;
+    await setAsync()(key, JSON.stringify(stock));
+}
+
+const getCurrentReservedStockById = async (itemId) => {
+    console.log(itemId);
+    const stock = await getAsync()(`item.${itemId}`);
+    console.log(stock);
+    return stock;
+
+};
+
+app.get('/list_products', (req, res) => {
+  const object = listProducts.map((item) => ({
+    itemId: item.Id,
+    itemName: item.name,
+    price: item.price,
+    initialAvailableQuantity: item.stock,
+  }));
+  res.json(object);
+});
+
+app.get('/list_products/:itemId', async (req, res) => {
+  const itemId = req.params.itemId;
+    let stock = await getCurrentReservedStockById(itemId);
+    if (stock) {
+      stock = JSON.parse(stock);
+      res.json({
+        itemId: stock.Id,
+        itemName: stock.name,
+        price: stock.price,
+        initialAvailableQuantity: stock.stock,
+        currentQuantity: stock.stock,
+      });
+    } else {
+      res.json({ status: 'Product not found' })
+    }
+});
+
+app.get('/reserve_product/:itemId', async (req, res) => {
+  const itemId = +req.params.itemId;
+  let items = getItemById(itemId);
+  if (items.length > 0) {
+    const item = items[0];
+    if (item.stock > 0) {
+      reserveStockById(itemId, item);
+      res.json({ status: 'Reservation confirmed', itemId });
+    } else {
+      res.json({ status: 'Not enough stock available', itemId });
+    }
+  } else {
+    res.json({ status: 'Product not found' });
+  }
+});
+
+app.listen('1245', () => {
+  console.log('The server is listening on 1245');
+});
